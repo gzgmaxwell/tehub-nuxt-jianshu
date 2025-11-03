096a23a2b1c694f162dbf068ecfeaa0d8609d77c
marlonchiu <Knight1898@outlook.com>
2025-11-03 16:31:15 +0800

    ✨ feat: 5-3 用户鉴权中间件

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,28 @@
+// @ts-ignore
+import jwt from 'jsonwebtoken'
+
+/***
+ * 1、获取token
+ * 2、判断token，如果有token，则处理token，之后再验证token值
+ */
 export default defineEventHandler(event => {
-  const token = getHeader(event, 'Authorization')
-  event.context.auth = { token: token }
+  //获取token值
+  let token = getHeader(event, 'Authorization')
+  console.log('token', token)
+  if (token) {
+    // 处理token
+    token = token.replace('Bearer ', '')
+    // 秘钥
+    let secret = 'eifuisedfuvs'
+    try {
+      // 验证token
+      var decoded = jwt.verify(token, secret)
+      console.log("🚀 ~ decoded:", decoded)
+      console.log('1111', decoded.data.data.uid)
+      // 传递给上下文
+      event.context.auth = { uid: decoded.data.data.uid }
+    } catch (e) {
+      console.log('jwt 解码错误', e)
+    }
+  }
 })
--- server/utils/helper/index.ts
+++ server/utils/helper/index.ts
@@ -2,3 +2,7 @@ export const responseJson = (code: number, msg: string, data: object) => {
   let resp = { code: code, msg: msg, data: data }
   return resp
 }
+
+export const getLoginUid = (event: any) => {
+  return event.context.auth ? event.context.auth.uid : 0
+}
