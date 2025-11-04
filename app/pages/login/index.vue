df53466d70ca3b5f7b8952e90fcd6e6da6fcf15f
marlonchiu <Knight1898@outlook.com>
2025-11-04 15:14:07 +0800

    🌈 style: clear page

--- app/pages/login/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <a-button type="primary">登录</a-button>
-  </div>
-</template>
-
-<script lang="ts" setup></script>
--- app/pages/users-[group]/[id].vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <p>{{ $route.params.group }} - {{ $route.params.id }}</p>
-</template>
-
-<script lang="ts" setup>
-const route = useRoute()
-console.log('🚀 ~ route:', route)
-</script>
