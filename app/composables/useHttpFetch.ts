7e826b8de2dfe18eada6ab08a917b93d6d620441
marlonchiu <Knight1898@outlook.com>
2025-11-04 15:03:52 +0800

    ✨ feat: 注册登录页面开发

--- app/composables/useHttpFetch.ts
+++ app/composables/useHttpFetch.ts
@@ -3,29 +3,30 @@ interface MyFetchOptions {
   [key: string]: any
 }
 export const useHttpFetch = (url: string, opt: MyFetchOptions) => {
-  const token = useCookie('token')
+  const token = useCookie('accessToken')
   // 添加请求头 token
   const headers = {
     ...opt.headers,
-    ...(token ? { Authorization: `Bearer ${token}` } : {})
+    ...(token.value ? { Authorization: `Bearer ${token.value}` } : {})
   }
   opt.headers = headers
 
   return useFetch(url, {
     ...opt,
-    baseURL: '',
+    baseURL: 'http://localhost:3000/',
     onRequest({ request, options }) {
       console.log('🚀 ~ onRequest ~ request:', request)
     },
     onRequestError({ request, options, error }) {
-      console.log('🚀 ~ onRequestError ~ request:', request)
       // Handle the request errors
     },
     onResponse({ request, response, options }) {
       // Process the response data
+      console.log("🚀 ~ onResponse ~ response:", response)
     },
     onResponseError({ request, response, options }) {
       // Handle the response errors
+      console.log('error', response.status)
     }
   })
 }
@@ -33,4 +34,13 @@ export const useHttpFetch = (url: string, opt: MyFetchOptions) => {
 // 定义接口
 export const userInfoFetch = (opt: MyFetchOptions) => {
   return useHttpFetch('/api/user/info', opt)
+}
+
+// 注册
+export const registerFetch = (opt: MyFetchOptions) => {
+  return useHttpFetch('/api/auth/register', opt)
+}
+// 登录
+export const loginFetch = (opt: MyFetchOptions) => {
+  return useHttpFetch('/api/auth/login', opt)
 }
